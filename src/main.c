f989621f447ebc19688cf7c5c7bdd0c444a82de6
Brandon Hunt <134429684+brandonhunt00@users.noreply.github.com>
2024-11-05 15:56:27 -0300

    Update main.c
    adicao de uma raquete, mudanca na posicao das raquetes e criacao de um placar rudimentar

--- src/main.c
+++ src/main.c
@@ -3,102 +3,111 @@
 #include "keyboard.h"
 #include "timer.h"
 
+#define ALTURA_DAS_RAQUETES 5
+#define CORRECAO_RAQUETE 1  
+
+typedef struct {
+    int x, y;
+} Raquete;
+
 int bolaX = 10, bolaY = 5;
 int direcaoX = 1, direcaoY = 1;
-int paddleX = 40; 
-int paddleY = 20; 
-int paddleWidth = 6;  
-int pontuacao = 0;
+Raquete raquete1 = {1, 10};  
+Raquete raquete2 = {78, 10}; 
+int pontuacao1 = 0, pontuacao2 = 0;
 
 void printPontuacao() {
     screenSetColor(WHITE, DARKGRAY);
-    screenGotoxy(0, 0);  
-    printf("Pontuação: %d", pontuacao);
+    screenGotoxy(0, 0);
+    printf("Jogador 1: %d | Jogador 2: %d", pontuacao1, pontuacao2);
 }
 
-void printRaquete(int nextPaddleX) {
+void printRaquete(Raquete *raquete) {
     screenSetColor(GREEN, DARKGRAY);
-    screenGotoxy(paddleX, paddleY);
-    printf("      ");  
+    for (int i = 0; i < ALTURA_DAS_RAQUETES; i++) {
+        screenGotoxy(raquete->x, raquete->y + i);
+        printf("|");
+    }
+}
 
-    paddleX = nextPaddleX;
-
-    screenGotoxy(paddleX, paddleY);
-    printf("======"); 
+void clearRaquete(Raquete *raquete) {
+    for (int i = 0; i < ALTURA_DAS_RAQUETES; i++) {
+        screenGotoxy(raquete->x, raquete->y + i);
+        printf(" ");
+    }
 }
 
 void printBola(int nextBolaX, int nextBolaY) {
     screenSetColor(WHITE, DARKGRAY);
     screenGotoxy(bolaX, bolaY);
-    printf(" ");  
+    printf(" ");
 
     bolaX = nextBolaX;
     bolaY = nextBolaY;
 
     screenGotoxy(bolaX, bolaY);
-    printf("O"); 
-}
-
-void printKey(int ch) {
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code: ");
-
-    screenGotoxy(34, 23);
-    printf("            ");
-    
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
-
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
-    }
+    printf("O");
 }
 
 int main() {
 
     int ch = 0;
-
     screenInit(1);
     keyboardInit();
     timerInit(50);
-    printRaquete(paddleX); 
+
     printPontuacao();
+    printRaquete(&raquete1);
+    printRaquete(&raquete2);
     screenUpdate();
 
     while (ch != 10) {
         if (keyhit()) {
             ch = readch();
-            printKey(ch);
-            
-            if (ch == 'a' && paddleX > MINX+1) {
-                printRaquete(paddleX - 2);  
-            }
-            else if (ch == 'd' && paddleX < MAXX - paddleWidth - 1) {
-                printRaquete(paddleX + 2); 
+            if (ch == 'w' && raquete1.y > 1) {
+                clearRaquete(&raquete1);
+                raquete1.y -= CORRECAO_RAQUETE;
+            } else if (ch == 's' && raquete1.y < SCRENDY - ALTURA_DAS_RAQUETES) {
+                clearRaquete(&raquete1);
+                raquete1.y += CORRECAO_RAQUETE;
+            } else if (ch == 'i' && raquete2.y > 1) {
+                clearRaquete(&raquete2);
+                raquete2.y -= CORRECAO_RAQUETE;
+            } else if (ch == 'k' && raquete2.y < SCRENDY - ALTURA_DAS_RAQUETES) {
+                clearRaquete(&raquete2);
+                raquete2.y += CORRECAO_RAQUETE;
             }
             screenUpdate();
         }
+
         if (timerTimeOver() == 1) {
             int novaBolaX = bolaX + direcaoX;
-            if (novaBolaX >= MAXX-1 || novaBolaX <= MINX+1) direcaoX = -direcaoX;
-
             int novaBolaY = bolaY + direcaoY;
-            
-            if (novaBolaY == paddleY && novaBolaX >= paddleX && novaBolaX <= (paddleX + paddleWidth)) {
-                direcaoY = -direcaoY; 
-                pontuacao++; 
-                printPontuacao();  
-            }
-            else if (novaBolaY >= MAXY-1 || novaBolaY <= MINY+1) {
-                direcaoY = -direcaoY;  
+
+            if (novaBolaY <= 1 || novaBolaY >= SCRENDY - 1) direcaoY = -direcaoY;
+
+            if ((novaBolaX == raquete1.x + 1 && novaBolaY >= raquete1.y && novaBolaY < raquete1.y + ALTURA_DAS_RAQUETES) ||
+                (novaBolaX == raquete2.x - 1 && novaBolaY >= raquete2.y && novaBolaY < raquete2.y + ALTURA_DAS_RAQUETES)) {
+                direcaoX = -direcaoX;
             }
 
-            printBola(novaBolaX, novaBolaY);  
+            if (novaBolaX <= 0) {
+                pontuacao2++;
+                novaBolaX = SCRENDX / 2;
+                novaBolaY = SCRENDY / 2;
+                direcaoX = -direcaoX;
+                printPontuacao();
+            } else if (novaBolaX >= SCRENDX) {
+                pontuacao1++;
+                novaBolaX = SCRENDX / 2;
+                novaBolaY = SCRENDY / 2;
+                direcaoX = -direcaoX;
+                printPontuacao();
+            }
 
-            printRaquete(paddleX);  
+            printBola(novaBolaX, novaBolaY);
+            printRaquete(&raquete1);
+            printRaquete(&raquete2);
             screenUpdate();
         }
     }
