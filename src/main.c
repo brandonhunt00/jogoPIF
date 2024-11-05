8d25360430e4d048b8517f144803be9707bf6db5
Brandon Hunt <134429684+brandonhunt00@users.noreply.github.com>
2024-11-05 15:47:44 -0300

    Update main.c
    remocao do hello world

--- src/main.c
+++ src/main.c
@@ -3,8 +3,6 @@
 #include "keyboard.h"
 #include "timer.h"
 
-int x = 34, y = 12;
-int incX = 1, incY = 1;
 int bolaX = 10, bolaY = 5;
 int direcaoX = 1, direcaoY = 1;
 int paddleX = 40; 
@@ -18,7 +16,7 @@ void printPontuacao() {
     printf("Pontuação: %d", pontuacao);
 }
 
-void printPaddle(int nextPaddleX) {
+void printRaquete(int nextPaddleX) {
     screenSetColor(GREEN, DARKGRAY);
     screenGotoxy(paddleX, paddleY);
     printf("      ");  
@@ -29,7 +27,7 @@ void printPaddle(int nextPaddleX) {
     printf("======"); 
 }
 
-void printBall(int nextBolaX, int nextBolaY) {
+void printBola(int nextBolaX, int nextBolaY) {
     screenSetColor(WHITE, DARKGRAY);
     screenGotoxy(bolaX, bolaY);
     printf(" ");  
@@ -39,16 +37,6 @@ void printBall(int nextBolaX, int nextBolaY) {
 
     screenGotoxy(bolaX, bolaY);
     printf("O"); 
-}
-
-void printHello(int nextX, int nextY) {
-    screenSetColor(CYAN, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("Hello World");
 }
 
 void printKey(int ch) {
@@ -71,14 +59,13 @@ void printKey(int ch) {
 
 int main() {
 
-    static int ch = 0;
+    int ch = 0;
 
     screenInit(1);
     keyboardInit();
     timerInit(50);
-
-    printHello(x, y);
-    printPaddle(paddleX); 
+    printRaquete(paddleX); 
+    printPontuacao();
     screenUpdate();
 
     while (ch != 10) {
@@ -86,45 +73,32 @@ int main() {
             ch = readch();
             printKey(ch);
             
-            if (ch == 'a' && paddleX > MINX+1) 
-            {
-                printPaddle(paddleX - 2);  
+            if (ch == 'a' && paddleX > MINX+1) {
+                printRaquete(paddleX - 2);  
             }
-            else if (ch == 'd' && paddleX < MAXX - paddleWidth - 1) 
-            {
-                printPaddle(paddleX + 2); 
+            else if (ch == 'd' && paddleX < MAXX - paddleWidth - 1) {
+                printRaquete(paddleX + 2); 
             }
-
             screenUpdate();
         }
-
         if (timerTimeOver() == 1) {
             int novaBolaX = bolaX + direcaoX;
             if (novaBolaX >= MAXX-1 || novaBolaX <= MINX+1) direcaoX = -direcaoX;
 
             int novaBolaY = bolaY + direcaoY;
             
-            if (novaBolaY == paddleY && novaBolaX >= paddleX && novaBolaX <= (paddleX + paddleWidth))
-            {
+            if (novaBolaY == paddleY && novaBolaX >= paddleX && novaBolaX <= (paddleX + paddleWidth)) {
                 direcaoY = -direcaoY; 
                 pontuacao++; 
                 printPontuacao();  
             }
-            else if (novaBolaY >= MAXY-1 || novaBolaY <= MINY+1)
-            {
+            else if (novaBolaY >= MAXY-1 || novaBolaY <= MINY+1) {
                 direcaoY = -direcaoY;  
             }
 
-            printBall(novaBolaX, novaBolaY);  
+            printBola(novaBolaX, novaBolaY);  
 
-            int novaPosX = x + incX;
-            if (novaPosX >= (MAXX - strlen("Hello World") - 1) || novaPosX <= MINX + 1) incX = -incX;
-
-            int novaPosY = y + incY;
-            if (novaPosY >= MAXY - 1 || novaPosY <= 1) incY = -incY;
-
-            printHello(novaPosX, novaPosY);
-            printPaddle(paddleX);  
+            printRaquete(paddleX);  
             screenUpdate();
         }
     }
